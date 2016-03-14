fcac580171d543a328bdcdc60233dde2d5643d72
SoHirai <So.Hirai.16@gmail.com>
2016-03-15 00:20:59 +0900

    edit view controller as access initial homepage

--- SimpleBrowser/SimpleBrowser/ViewController.swift
+++ SimpleBrowser/SimpleBrowser/ViewController.swift
@@ -15,14 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var reloadButton: UIBarButtonItem!
     @IBOutlet weak var stopButton: UIBarButtonItem!
 
+    // initial homepage
+    let homeUrl = "http://www.yahoo.co.jp"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // view homepage
+        openUrl(homeUrl)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // open url with web view
+    func openUrl(urlString: String){
+        let url = NSURL(string: urlString)
+        let urlRequest = NSURLRequest(URL: url!)
+        webView.loadRequest(urlRequest)
     }
 
     @IBAction func backButtonTapped(sender: UIBarButtonItem) {
